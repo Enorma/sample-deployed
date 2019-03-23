61c0fcade824f9747e01635359e12712dc2eecf6
Enorma <enorma29@gmail.com>
2019-03-22 21:15:28 -0600

    added database

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	port = process.env.PORT || 3000,
+	databaseurl = process.env.DBCONN || "mongodb://localhost:27017/academy"
+};
+
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const config = require("./config");
+
+const app = express();
+
+const port = config.port;
+const dbconn = config.databaseurl;
+
+mongoose.connect(dbconn, {useNewUrlParser: true})
+	.then(
+		() => {
+			console.log("JÁLESEEEEE PUTOOOO");
+		},
+		err => {
+			console.log("NO VAAATO");
+		}
+	);
+//mongoose.connect
+
+app.all("/", (req,res) => {
+	res.json({
+		api: "Centraal",
+		version: "0.0.1"
+	});
+});
+
 app.listen(port, () => console.log("Corriendo en puerto: "+port+"!"));
